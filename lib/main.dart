346e1a1f47b570c80321f70aea253a65bc18452d
Leo <lcheung@swellbottle.com>
2021-12-24 16:27:37 -0500

    create Question class for both question and answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,16 +29,30 @@ class _QuizPageState extends State<QuizPage> {
   // create empty list or array of Icon
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
+  // List<String> questions = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.',
+  // ];
+  //
+  // List<bool> answers = [
+  //   false,
+  //   true,
+  //   true,
+  // ];
 
-  List<bool> answers = [
-    false,
-    true,
-    true,
+  // Question q1 = new Question(
+  //     q: 'You can lead a cow down stairs but not up stairs.', a: false);
+  // Question q2 = new Question(
+  //     q: 'Approximately one quarter of human bones are in the feet.', a: true);
+  // Question q3 = new Question(q: 'A slug\'s blood is green.', a: true);
+
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
   int questionNumber = 0;
